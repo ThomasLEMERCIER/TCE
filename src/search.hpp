ee0067a3cd7cf2eee5398aaf3d029e00928c45e3
ThomasLEMERCIER <t.lemercier27@gmail.com>
2023-08-21 01:52:02 +0200

    STC Elo difference: 74.1 +/- 57.8, LOS: 99.4 %, DrawRatio: 31.0 % 101 of 1000 games finished.
    LTC
    Elo difference: 88.7 +/- 73.0, LOS: 99.2 %, DrawRatio: 35.0 %
    60 of 1000 games finished.

--- src/search.hpp
+++ src/search.hpp
@@ -27,14 +27,27 @@ struct ThreadData {
 void start_search(const Position& pos, int depth, const SearchLimits& limits);
 void stop_search();
 
-int quiescence(Position* pos, Score alpha, Score beta, ThreadData& td);
-int negamax(Position* pos, Score alpha, Score beta, int depth, bool null_pruning, ThreadData& td);
+Score quiescence(Position* pos, Score alpha, Score beta, ThreadData& td);
+Score negamax(Position* pos, Score alpha, Score beta, int depth, bool null_pruning, ThreadData& td);
+Score aspiration_window(Position* pos, Score previous_score, int depth, ThreadData& td);
 
 void search_position(ThreadData& td);
+constexpr int check_every_nodes = 2046;
 
-// search hyperparameters
+// =========== search hyperparameters ===========
+// === LMR ===
 constexpr int full_depth_moves = 4;
 constexpr int lmr_reduction = 3;
+
+// === Null move pruning ===
 constexpr int null_move_reduction = 3;
 
-constexpr int check_every_nodes = 2046;
+// === Aspiration window ===
+constexpr int aspiration_window_depth = 5;
+constexpr Score aspiration_window_start = 150;
+constexpr Score aspiration_window_decrement = 0;
+constexpr Score aspiration_window_end = 10;
+constexpr Score aspiration_window_max = 350;
+// ===============================================
+
+
